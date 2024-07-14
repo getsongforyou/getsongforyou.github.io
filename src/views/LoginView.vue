3dafc617302c7494a618a3db97ea3bbf45c18725
moyi <2012524820@qq.com>
2024-07-14 15:48:31 +0800

    项目初始化

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QingMengLoginView',
+
+  data() {
+    return {
+      user: '',
+      password: ''
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
