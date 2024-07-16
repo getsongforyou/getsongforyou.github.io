799ccc152f1bd04ffbd434b270366443b300bcf4
moyi <2012524820@qq.com>
2024-07-16 16:45:28 +0800

    layout bug fixed

--- src/views/example/TreeView.vue
+++ src/views/example/TreeView.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="tree">
+    <el-input placeholder="输入关键字进行过滤" v-model="filterText"> </el-input>
     <el-tree
       :data="data"
       show-checkbox
       node-key="id"
       :default-expanded-keys="[2, 3]"
       :default-checked-keys="[5]"
+      :filter-node-method="filterNode"
+      ref="tree"
     >
     </el-tree>
   </div>
 </template>
 
 <script>
-import { getTree } from '@/api/treeapi';
+import { getTree } from "@/api/treeapi";
 
 export default {
   name: "QingMengTreeView",
 
   data() {
     return {
-        data: null
+      data: null,
+      filterText: ''
     };
   },
+  watch: {
+      filterText(val) {
+        this.$refs.tree.filter(val);
+      }
+    },
 
-  created(){
-    getTree().then(
-        res=>{
-            this.data = res.data.data.tree
-        }
-    )
+  created() {
+    getTree().then((res) => {
+      this.data = res.data.data.tree;
+    });
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    filterNode(value, data) {
+        if (!value) return true;
+        return data.label.indexOf(value) !== -1;
+      }
+  },
 };
 </script>
